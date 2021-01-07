36a7a79e59327f6f0bdb71d16a7c1dbb0b6aca6b
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2021-01-08 01:56:27 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,5 @@
-# 3 решение
- 
 q = int(input())
 
-t = lambda q: 5 if q == 3 else (10 if q == 5 else (15 if q == 10 else 123))
-print(t(6))
+t = lambda q: 'Fizz' if q % 3 == 0 else ('Buzz' if q % 5 == 0 else ('FizzBuzz' if q % 3 == 0 and i % 5 == 0 else q))
+print([t(q) for q in range(1,101)])
 
